488d35742ec4946c13b84d8e633bfa25d8db71fe
iusmac <iusico.maxim@libero.it>
2025-03-24 11:15:06 +0100

    Revert "Import Google Settings Intelligence app prebuilt"
    This reverts commit 1a0c12ebcc86c57404989d1248554d65c51f925b.
    
    Also, this commit squahes the following related commits:
        Revert "rova: Update Google Settings Intelligence from redfin `UP1A.231005.007`"
    
        This reverts commit e53ff49bcd46a52091c05c121327a47f34d7228e.
    
        ---
    
        Revert "rova: Presigned apks with targetSdkVersion >= 30 must set `preprocessed: true`"
    
        This reverts commit d9376f466c15060ec0efff4c476fa72241f5e6a4.

--- map/misc.mk
+++ map/misc.mk
@@ -23,10 +23,6 @@ $(call copy-vendor-files, \
 )
 # }}}
 
-# Google Settings Intelligence app
-PRODUCT_PACKAGES += \
-    SettingsIntelligenceGooglePrebuilt
-
 # Subsystem Restart {{{
 $(call copy-vendor-files, \
     bin/ssr_diag \
